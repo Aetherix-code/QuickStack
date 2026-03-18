6860e1768334c8a85f8d63199dfd12714a00e2a4
Jerry VDP <jerry@djint.net>
2026-03-18 17:28:04 +0000

    Better env vars parsing

--- src/server/utils/env-var.utils.ts
+++ src/server/utils/env-var.utils.ts
@@ -3,8 +3,12 @@ import { AppExtendedModel } from "@/shared/model/app-extended.model";
 export class EnvVarUtils {
     static parseEnvVariables(app: AppExtendedModel) {
         return app.envVars ? app.envVars.split('\n').filter(x => !!x).map(env => {
-            const [name] = env.split('=');
-            const value = env.replace(`${name}=`, '');
+            let [name] = env.split('=');
+            name = name.trim();
+            let value = env.replace(`${name}=`, '').trim();
+            if (value.startsWith('"') && value.endsWith('"')) {
+                value = value.slice(1, -1);
+            }
             return { name, value };
         }) : [];
     }
