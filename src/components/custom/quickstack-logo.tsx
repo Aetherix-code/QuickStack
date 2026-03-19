54e62bdb8ede2ac6fc4bb5b7273c0deed9d35ee0
Jerry VDP <jerry@djint.net>
2026-03-19 09:27:40 +0000

    Fix FE warning

--- src/components/custom/quickstack-logo.tsx
+++ src/components/custom/quickstack-logo.tsx
@@ -1,6 +1,6 @@
 
 const defaultLight = (className?: string) => <svg className={className} xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24" fill="none"
-    stroke="#f9fafb" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round">
+    stroke="#f9fafb" strokeWidth="1.2" stroke-linecap="round" stroke-linejoin="round">
     <path d="m17 16.5-5-3" />
     <path d="m7 16.5-4.74-2.85" />
     <path stroke="#25c2a0" d="M12 8 7.26 5.15" />
@@ -21,7 +21,7 @@ const defaultLight = (className?: string) => <svg className={className} xmlns="h
 </svg>;
 
 const defaultDark = (className?: string) => <svg className={className} xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24" fill="none"
-    stroke="#444950" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round">
+    stroke="#444950" strokeWidth="1.2" stroke-linecap="round" stroke-linejoin="round">
     <path d="m17 16.5-5-3" />
     <path d="m7 16.5-4.74-2.85" />
     <path stroke="#25c2a0" d="M12 8 7.26 5.15" />
@@ -42,7 +42,7 @@ const defaultDark = (className?: string) => <svg className={className} xmlns="ht
 </svg>;
 
 const allLight = (className?: string) => <svg className={className} xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 24 24" fill="none"
-    stroke="#f9fafb" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round">
+    stroke="#f9fafb" strokeWidth="1.2" stroke-linecap="round" stroke-linejoin="round">
     <path d="m17 16.5-5-3" />
     <path d="m7 16.5-4.74-2.85" />
     <path d="M12 8 7.26 5.15" />
